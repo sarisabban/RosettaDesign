ac15429c0638c4e84bd37f3096885e61b1d08e44
Sari Sabban <sari.sabban@gmail.com>
2017-07-06 14:54:22 +0100

    Update RosettaDesign.py

--- RosettaDesign.py
+++ RosettaDesign.py
@@ -3,10 +3,15 @@
 import numpy , Bio.PDB , os , sys
 
 from pyrosetta import *
+from rosetta import *
 from pyrosetta.toolbox import *
 from rosetta.protocols.relax import *
 from rosetta.protocols.moves import *
 from rosetta.protocols.simple_moves import *
+from rosetta.protocols.denovo_design.movers import *
+from rosetta.core.pack.task import *
+from rosetta.core.scoring.packstat import *
+from rosetta.core.pack.task.operation import *
 from rosetta.core.pack.task import TaskFactory
 init()
 
@@ -14,13 +19,13 @@ def Relax(pose):
 	''' Relaxes a structure '''
 	''' Updates the original pose with the relaxed pose '''
 	scorefxn = get_fa_scorefxn()
-	relax = ClassicRelax()
+	relax = FastRelax()
 	relax.set_scorefxn(scorefxn)
 	relax.apply(pose)
 
 def SASA(pose):
-	''' Calculates the different layers (Surface, Boundery, Core) of a structure according its SASA (solvent-accessible surface area) '''
-	''' Returns three lists Surface amino acids = [0] , Boundery amino acids = [1] , Core amino acids = [2] '''
+	''' Calculates the different layers (Surface, Boundary, Core) of a structure according its SASA (solvent-accessible surface area) '''
+	''' Returns three lists Surface amino acids = [0] , Boundary amino acids = [1] , Core amino acids = [2] '''
 	#Temporary generate a .pdb file of the pose to isolate the layers since it is not yet possible to do that using a Rosetta pose, this temporary .pdb file will be deleted after the layers are found
 	pose.dump_pdb('ToDesign.pdb')
 	#Standard script to setup biopython's DSSP to calculate SASA using Wilke constants
@@ -55,113 +60,178 @@ def SASA(pose):
 		lis.append((x[2] , sasa))
 	#Label each amino acid depending on its SASA position according to the parameters highlighted in the paper by (Koga et.al., 2012 - PMID: 23135467). The parameters are as follows:
 	#Surface:	Helix or Sheet: SASA => 60		Loop: SASA => 40
-	#Boundery:	Helix or Sheet: 15 < SASA < 60		Loop: 25 < SASA < 40
+	#Boundary:	Helix or Sheet: 15 < SASA < 60		Loop: 25 < SASA < 40
 	#Core:		Helix or Sheet: SASA =< 15		Loop: SASA =< 25	
 	surface = list()
-	boundery = list()
+	boundary = list()
 	core = list()
 	count = 0
 	for x , y in lis:
 		count = count + 1
-		if y <= 25 and (x == '-' or x == 'T' or x == 'S'):			#Loop (DSSP code is - or T or S)
+		if y <= 25 and (x == '-' or x == 'T' or x == 'S'):				#Loop (DSSP code is - or T or S)
 			core.append(count)
-		elif 25 < y < 40 and (x == '-' or x == 'T' or x == 'S'):		#Loop (DSSP code is - or T or S)
-			boundery.append(count)
-		elif y >= 40 and (x == '-' or x == 'T' or x == 'S'):			#Loop (DSSP code is - or T or S)
+		elif 25 < y < 40 and (x == '-' or x == 'T' or x == 'S'):			#Loop (DSSP code is - or T or S)
+			boundary.append(count)
+		elif y >= 40 and (x == '-' or x == 'T' or x == 'S'):				#Loop (DSSP code is - or T or S)
 			surface.append(count)
-		elif y <= 15 and (x == 'G' or x == 'H' or x == 'I'):			#Helix (DSSP code is G or H or I)
+		elif y <= 15 and (x == 'G' or x == 'H' or x == 'I'):				#Helix (DSSP code is G or H or I)
 			core.append(count)
-		elif 15 < y < 60 and (x == 'G' or x == 'H' or x == 'I'):		#Helix (DSSP code is G or H or I)
-			boundery.append(count)
-		elif y >= 60 and (x == 'G' or x == 'H' or x == 'I'):			#Helix (DSSP code is G or H or I)
+		elif 15 < y < 60 and (x == 'G' or x == 'H' or x == 'I'):			#Helix (DSSP code is G or H or I)
+			boundary.append(count)
+		elif y >= 60 and (x == 'G' or x == 'H' or x == 'I'):				#Helix (DSSP code is G or H or I)
 			surface.append(count)
-		elif y <= 15 and (x == 'B' or x == 'E'):				#Sheet (DSSP code is B or E)
+		elif y <= 15 and (x == 'B' or x == 'E'):					#Sheet (DSSP code is B or E)
 			core.append(count)
-		elif 15 < y < 60 and (x == 'B' or x == 'E'):				#Sheet (DSSP code is B or E)
-			boundery.append(count)
-		elif y >= 60 and (x == 'B' or x == 'E'):				#Sheet (DSSP code is B or E)
+		elif 15 < y < 60 and (x == 'B' or x == 'E'):					#Sheet (DSSP code is B or E)
+			boundary.append(count)
+		elif y >= 60 and (x == 'B' or x == 'E'):					#Sheet (DSSP code is B or E)
 			surface.append(count)	
-	os.remove('ToDesign.pdb')							#Keep working directory clean
-	return(surface , boundery , core)
+	os.remove('ToDesign.pdb')								#Keep working directory clean
+	return(surface , boundary , core)
 
-def Design_Whole(pose):
-	''' Applies RosettaDesign to change the whole structure's amino acids (the whole structure all at once) while maintaining the same backbone '''
-	''' Generates the Designed.pdb file '''
-	#1 - Relax original structure
-	scorefxn = get_fa_scorefxn()							#Call the score function
-	score1_original_before_relax = scorefxn(pose)					#Measure score before relaxing
-	Relax(pose)									#Relax structure
-	score2_original_after_relax = scorefxn(pose)					#Measure score after relaxing
-	#2 - Preform RosettaDesign for whole structure
-	for inter in range(3):
-		task_pack = standard_packer_task(pose)
-		pack_mover = PackRotamersMover(scorefxn, task_pack)
-		pack_mover.apply(pose)
-		#3 - Relax pose
-		Relax(pose)
-	score3_of_design_after_relax = scorefxn(pose)					#Measure score of designed pose
-	pose.dump_pdb('Designed.pdb')							#Export final pose into a .pdb structure file
-	print(score1_original_before_relax)
-	print(score2_original_after_relax)
-	print(score3_of_design_after_relax)
+class Design():
+	def Whole(pose):
+		''' Applies RosettaDesign to change the whole structure's amino acids (the whole structure all at once) while maintaining the same backbone '''
+		''' Generates the Designed.pdb file '''
+		#A - Relax original structure
+		scorefxn = get_fa_scorefxn()							#Call the score function
+		score1_original_before_relax = scorefxn(pose)					#Measure score before relaxing
+		Relax(pose)									#Relax structure
+		score2_original_after_relax = scorefxn(pose)					#Measure score after relaxing
+		#B - Preform RosettaDesign for whole structure
+		for inter in range(3):
+			task_pack = standard_packer_task(pose)
+			pack_mover = PackRotamersMover(scorefxn, task_pack)
+			pack_mover.apply(pose)
+			#C - Relax Pose
+			Relax(pose)
+		#D - Output Result
+		score3_of_design_after_relax = scorefxn(pose)					#Measure score of designed pose
+		pose.dump_pdb('Designed.pdb')							#Export final pose into a .pdb structure file
+		print(score1_original_before_relax)
+		print(score2_original_after_relax)
+		print(score3_of_design_after_relax)
 
-def Design_Layer(pose):
-	''' Applies RosettaDesign to change the whole structure's amino acids (one layer at a time) while maintaining the same backbone. Should be more efficient and faster than the previous method (Design_Full) '''
-	''' Generates the Designed.pdb file '''
-	#Relax original structure
-	scorefxn = get_fa_scorefxn()							#Call the score function
-	score1_original_before_relax = scorefxn(pose)					#Measure score before relaxing
-	Relax(pose)									#Relax structure
-	score2_original_after_relax = scorefxn(pose)					#Measure score after relaxing
-	#Preform RosettaDesign one layer at a time
-	for inter in range(3):
-		#1 - Get SASA Layers
-		sasa = SASA(pose)
-		surface = sasa[0]
-		boundery = sasa[1]
-		core = sasa[2]
-		#2 - Preform RosettaDesign on each layer
-		#Design core
-		task_pack = standard_packer_task(pose)
-		pack_mover = PackRotamersMover(scorefxn , task_pack)
-		task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
-		for AA in core:
-			coreAA = pose.residue(AA).name()
-			if coreAA == 'CYS:disulfide':
-				continue
-			else:
-				task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
-		pack_mover.apply(pose)
-		#Design boundery
-		task_pack = standard_packer_task(pose)
-		pack_mover = PackRotamersMover(scorefxn , task_pack)
-		task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
-		for AA in boundery:
-			boundAA = pose.residue(AA).name()
-			if boundAA == 'CYS:disulfide':
-				continue
-			else:
-				task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
-		pack_mover.apply(pose)
-		#Design surface
-		task_pack = standard_packer_task(pose)
-		pack_mover = PackRotamersMover(scorefxn , task_pack)
-		task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
-		for AA in surface:
-			surfAA = pose.residue(AA).name()
-			if surfAA == 'CYS:disulfide':
-				continue
-			else:
-				task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
-		pack_mover.apply(pose)
-		#3 - Relax pose
-		Relax(pose)
-	score3_of_design_after_relax = scorefxn(pose)					#Measure score of designed pose
-	pose.dump_pdb('Designed.pdb')							#Export final pose into a .pdb structure file
-	print(score1_original_before_relax)
-	print(score2_original_after_relax)
-	print(score3_of_design_after_relax)
+	def Layer(pose):
+		''' Applies RosettaDesign to change the whole structure's amino acids (one layer at a time) while maintaining the same backbone. Should be more efficient and faster than the previous Whole method '''
+		''' Generates the Designed.pdb file '''
+		#A - Relax original structure
+		scorefxn = get_fa_scorefxn()							#Call the score function
+		score1_original_before_relax = scorefxn(pose)					#Measure score before relaxing
+		Relax(pose)									#Relax structure
+		score2_original_after_relax = scorefxn(pose)					#Measure score after relaxing
+		#B - Preform RosettaDesign one layer at a time
+		for inter in range(3):
+			#1 - Get SASA Layers
+			sasa = SASA(pose)
+			surface = sasa[0]
+			boundary = sasa[1]
+			core = sasa[2]
+			#2 - Preform RosettaDesign on each layer
+			#Design core
+			task_pack = standard_packer_task(pose)
+			pack_mover = PackRotamersMover(scorefxn , task_pack)
+			task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
+			for AA in core:
+				coreAA = pose.residue(AA).name()
+				if coreAA == 'CYS:disulfide':
+					continue
+				else:
+					task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
+			pack_mover.apply(pose)
+			#Design bosundery
+			task_pack = standard_packer_task(pose)
+			pack_mover = PackRotamersMover(scorefxn , task_pack)
+			task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
+			for AA in boundary:
+				boundAA = pose.residue(AA).name()
+				if boundAA == 'CYS:disulfide':
+					continue
+				else:
+					task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
+			pack_mover.apply(pose)
+			#Design surface
+			task_pack = standard_packer_task(pose)
+			pack_mover = PackRotamersMover(scorefxn , task_pack)
+			task_pack.temporarily_fix_everything()					#To prevent all amino acids from being designed
+			for AA in surface:
+				surfAA = pose.residue(AA).name()
+				if surfAA == 'CYS:disulfide':
+					continue
+				else:
+					task_pack.temporarily_set_pack_residue(AA , True)	#To move only spesific amino acids
+			pack_mover.apply(pose)
+			#C - Relax Pose
+			Relax(pose)
+		#D - Output Result
+		score3_of_design_after_relax = scorefxn(pose)					#Measure score of designed pose
+		pose.dump_pdb('Designed.pdb')							#Export final pose into a .pdb structure file
+		print('---------------------------------------------------------')
+		print('Original Structure Score:' , '\t' , score1_original_before_relax)
+		print('Relaxed Original Score:' , '\t' , score2_original_after_relax)
+		print('Relaxed Design Score:' , '\t\t' , score3_of_design_after_relax)
+
+	def Pack(pose):
+		''' Applies FastDesign to change the whole structure's amino acids (one layer at a time as well as designing towards an optimally packed core) while maintaining the same backbone. Should be faster than the Whole method and results in a better final structure than the Layer method '''
+		''' Generates the Designed.pdb file '''
+		#A - Relax original structure
+		scorefxn = get_fa_scorefxn()							#Call the score function
+		score1_original_before_relax = scorefxn(pose)					#Measure score before relaxing
+#		Relax(pose)									#Relax structure
+		score2_original_after_relax = scorefxn(pose)					#Measure score after relaxing
+		#B - FastDesign Protocol							#Uses Generic Monte Carlo with PackStat as a filter to direct FastDesign towards an optimally packed structure core
+		layers = [2 , 1 , 0]								#Layer Identity from SASA Surface = [0] , Boundary = [1] , Core = [2]
+		for identity in layers:								#Loop through each layer
+			#1 - Setup The PackStat Filter
+			filters = rosetta.protocols.simple_filters.PackStatFilter()
+			#2 - Identify The Layers
+			sasa = SASA(pose)							#Re-calculate SASA every time because amino acid position can change from one layer to another during the design phase, therefore make sure to design the layer not the amino acid
+			layer = sasa[identity]							#Changes every iteration to start with Core (sasa[2]) then Boundary (sasa[1]) then Surface (sasa[0])
+			#3 - Generate The Resfile						#Will generate a new Resfile for each layer (which is why it is deleted at the end of the loop)
+			Resfile = open('Resfile.resfile' , 'w')
+			Resfile.write('NATAA\n')
+			Resfile.write('start\n')
+			for line in layer:
+				Resfile.write(str(line) + ' A ALLAA\n')
+			Resfile.close()
+			#4 - Setup The FastDesign Mover
+			task = TaskFactory()
+			read = ReadResfile('Resfile.resfile')
+			task.push_back(read)
+			movemap = MoveMap()
+			movemap.set_bb(False)
+			movemap.set_chi(True)
+			mover = FastDesign()
+			mover.set_task_factory(task)
+			mover.set_movemap(movemap)
+			mover.set_scorefxn(scorefxn)
+			#5 - Setup and Apply The Generic Monte Carlo Mover
+			MC = GenericMonteCarloMover()							#Call Monter Carlo Class
+			MC.set_mover(mover)								#Load The Mover
+			MC.set_scorefxn(scorefxn)							#Set score function
+			MC.set_maxtrials(1)								#Set number of monte carlo loops
+			MC.set_temperature(1)								#Set temperature
+			MC.set_preapply(False)								#To apply Boltzmann accept/reject to all applications of the mover (always use False)
+			MC.set_drift(True)								#Make current pose = next iteration pose
+			MC.set_sampletype('high')							#Move monte carlo to higher filter score
+			MC.recover_low()								#True - at the end of application, the pose is set to the lowest (or highest if sample_type="high") scoring pose
+			MC.stopping_condition()								#Stops before trials are done if a filter evaluates to true
+			MC.task_factory(task)								#Include a Task Factory
+			MC.boltzmann(pose)								#Evaulates a pose based on the scores/filters + temperatures
+			MC.add_filter(filters , True , 1.0 , 'high' , True)				#Add a filter
+#			MC.apply(pose)									#Apply Move
+			os.remove('Resfile.resfile')							#To keeo working directory clean, and to make sure each Resfile has the info for each layer only and they do not get mixed together in one Resfile
+		#C - Relax Pose
+#		Relax(pose)									#Relax structure
+		#D - Output Result
+		score3_of_design_after_relax = scorefxn(pose)					#Measure score of designed pose
+		pose.dump_pdb('Designed.pdb')							#Export final pose into a .pdb structure file
+		print('---------------------------------------------------------')
+		print('Original Structure Score:' , '\t' , score1_original_before_relax)
+		print('Relaxed Original Score:' , '\t' , score2_original_after_relax)
+		print('Relaxed Design Score:' , '\t\t' , score3_of_design_after_relax)
 #------------------------------------------------------------------------------------------------------------------------------------
 pose = pose_from_pdb(sys.argv[1])
-#Design_Whole(pose)
-Design_Layer(pose)
+#Design.Whole(pose)
+#Design.Layer(pose)
+Design.Pack(pose)
